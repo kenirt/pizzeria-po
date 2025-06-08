dd17b1daaa35838bdac489d55180813c52bfd7cf
Antoni Zemo <antoni.zemlo@proton.me>
2025-06-08 14:21:35 +0200

    Do klasy Order metody AdvanceStatus ktora zamienia status zamowienia na kolejny

--- pizzeria/pizzeria/Models/Order.cs
+++ pizzeria/pizzeria/Models/Order.cs
@@ -1,4 +1,5 @@
 ﻿using pizzeria.Enums;
+using pizzeria.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,49 @@ namespace pizzeria.Models
         public int Id;
         public DateTime TimePlaced;
         public OrderStatus Status;
+        public IPizza Pizza;
         public decimal FinalPrice;
-    }
+        public Order(int id, IPizza pizza)
+        {
+            Id = id;
+            Pizza = pizza;
+            TimePlaced = DateTime.Now;
+            Status = OrderStatus.Pending;
+            FinalPrice = pizza.CalculatePrice();
+        }
+
+        public void Cancel()
+        {
+            if (Status == OrderStatus.Pending)
+            {
+                Status = OrderStatus.Cancelled;
+            }
+            else
+            {
+                Console.WriteLine("Cannot cancel order");
+            }
+        }
+
+        public void AdvanceStatus()
+        {
+            if(Status == OrderStatus.Cancelled)
+            {
+                throw new InvalidOperationException();
+            }
+            switch (Status)
+            {
+                case OrderStatus.Pending:
+                    Status = OrderStatus.InPreparation;
+                    break;
+                case OrderStatus.InPreparation:
+                    Status = OrderStatus.Ready;
+                    break;
+                case OrderStatus.Ready:
+                    Status = OrderStatus.Ready;
+                    break;
+                default:
+                    break;
+
+            }
+        }
 }
